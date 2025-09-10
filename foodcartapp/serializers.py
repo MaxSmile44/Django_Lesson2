799dec1a5e764b81e512053c981d4fadece8a224
MaxSmile44 <afmaxim89@gmail.com>
2025-09-10 12:59:20 +0300

    Moved serializers in serializers.py

---
+++ foodcartapp/serializers.py
@@ -0,0 +1,90 @@
+import phonenumbers
+import requests
+
+from django.conf import settings
+from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer, ValidationError
+from .models import Order, OrderProduct, Product
+from coordinates.models import Coordinate
+
+
+class OrderProductSerializer(ModelSerializer):
+    product = serializers.IntegerField()
+
+    class Meta:
+        model = OrderProduct
+        fields = ['product', 'quantity']
+
+    def validate_product(self, value):
+        if not Product.objects.filter(id=value).exists():
+            raise ValidationError(f'Продукт с id={value} не существует')
+        return value
+
+
+class OrderSerializer(ModelSerializer):
+    products = OrderProductSerializer(many=True, allow_empty=False, write_only=True)
+    phonenumber = serializers.CharField(source='phone')
+
+    class Meta:
+        model = Order
+        fields = ['firstname', 'lastname', 'phonenumber', 'address', 'products']
+
+    def validate_phonenumber(self, value):
+        phone = phonenumbers.parse(value, 'RU')
+        if not phonenumbers.is_valid_number(phone):
+            raise ValidationError([f"Invalid phonenumber: {value}"])
+        return value
+
+    def create(self, validated_data):
+        order = Order.objects.create(
+            firstname=validated_data['firstname'],
+            lastname=validated_data['lastname'],
+            phone=validated_data['phone'],
+            address=validated_data['address']
+        )
+
+        coordinates = Coordinate.objects.all()
+        order_addresses = [coordinate.address for coordinate in coordinates]
+
+        if validated_data['address'] not in order_addresses:
+            def fetch_coordinates(address):
+                apikey = settings.YANDEX_APIKEY
+                try:
+                    base_url = "https://geocode-maps.yandex.ru/1.x"
+                    response = requests.get(base_url, params={
+                        "geocode": address,
+                        "apikey": apikey,
+                        "format": "json",
+                    })
+                    response.raise_for_status()
+                    found_places = response.json()['response']['GeoObjectCollection']['featureMember']
+
+                    if not found_places:
+                        return None
+
+                    most_relevant = found_places[0]
+                    lon, lat = most_relevant['GeoObject']['Point']['pos'].split(" ")
+                    return lon, lat
+                except requests.exceptions.HTTPError as error:
+                    print(f'HTTPError: {error}')
+                    return None
+
+            lon, lat = fetch_coordinates(validated_data['address'])
+            coordinates.create(
+                address=validated_data['address'],
+                lon=lon,
+                lat=lat
+            )
+
+        products_ids = [product['product'] for product in validated_data['products']]
+        products = Product.objects.filter(pk__in=products_ids)
+        product_map = {product.pk: product for product in products}
+        for product in validated_data['products']:
+            product_obj = product_map.get(product['product'])
+            OrderProduct.objects.create(
+                order=order,
+                product=product_obj,
+                quantity=product['quantity'],
+                price=product_obj.price
+            )
+        return order
--- foodcartapp/views.py
+++ foodcartapp/views.py
@@ -1,18 +1,11 @@
-import phonenumbers
-import requests
-
-from django.conf import settings
-from django.core.exceptions import ObjectDoesNotExist
 from django.db import transaction
 from django.http import JsonResponse
 from django.templatetags.static import static
-from rest_framework import status, serializers
 from rest_framework.decorators import api_view
-from rest_framework.serializers import ModelSerializer, ValidationError
 from rest_framework.response import Response
 
-from .models import Order, OrderProduct, Product
-from coordinates.models import Coordinate
+from .models import Order, Product
+from .serializers import OrderSerializer
 
 
 def banners_list_api(request):
@@ -67,88 +60,6 @@ def product_list_api(request):
     })
 
 
-class OrderProductSerializer(ModelSerializer):
-    product = serializers.IntegerField()
-
-    class Meta:
-        model = OrderProduct
-        fields = ['product', 'quantity']
-
-    def validate_product(self, value):
-        if not Product.objects.filter(id=value).exists():
-            raise ValidationError(f'Продукт с id={value} не существует')
-        return value
-
-
-class OrderSerializer(ModelSerializer):
-    products = OrderProductSerializer(many=True, allow_empty=False, write_only=True)
-    phonenumber = serializers.CharField(source='phone')
-
-    class Meta:
-        model = Order
-        fields = ['firstname', 'lastname', 'phonenumber', 'address', 'products']
-
-    def validate_phonenumber(self, value):
-        phone = phonenumbers.parse(value, 'RU')
-        if not phonenumbers.is_valid_number(phone):
-            raise ValidationError([f"Invalid phonenumber: {value}"])
-        return value
-
-    def create(self, validated_data):
-        order = Order.objects.create(
-            firstname=validated_data['firstname'],
-            lastname=validated_data['lastname'],
-            phone=validated_data['phone'],
-            address=validated_data['address']
-        )
-
-        coordinates = Coordinate.objects.all()
-        order_addresses = [coordinate.address for coordinate in coordinates]
-
-        if validated_data['address'] not in order_addresses:
-            def fetch_coordinates(address):
-                apikey = settings.YANDEX_APIKEY
-                try:
-                    base_url = "https://geocode-maps.yandex.ru/1.x"
-                    response = requests.get(base_url, params={
-                        "geocode": address,
-                        "apikey": apikey,
-                        "format": "json",
-                    })
-                    response.raise_for_status()
-                    found_places = response.json()['response']['GeoObjectCollection']['featureMember']
-
-                    if not found_places:
-                        return None
-
-                    most_relevant = found_places[0]
-                    lon, lat = most_relevant['GeoObject']['Point']['pos'].split(" ")
-                    return lon, lat
-                except requests.exceptions.HTTPError as error:
-                    print(f'HTTPError: {error}')
-                    return None
-
-            lon, lat = fetch_coordinates(validated_data['address'])
-            coordinates.create(
-                address=validated_data['address'],
-                lon=lon,
-                lat=lat
-            )
-
-        products_ids = [product['product'] for product in validated_data['products']]
-        products = Product.objects.filter(pk__in=products_ids)
-        product_map = {product.pk: product for product in products}
-        for product in validated_data['products']:
-            product_obj = product_map.get(product['product'])
-            OrderProduct.objects.create(
-                order=order,
-                product=product_obj,
-                quantity=product['quantity'],
-                price=product_obj.price
-            )
-        return order
-
-
 @api_view(['GET', 'POST'])
 def register_order(request):
     with transaction.atomic():
@@ -158,7 +69,7 @@ def register_order(request):
         elif request.method == 'POST':
             serializer = OrderSerializer(data=request.data)
             serializer.is_valid(raise_exception=True)
-            order = serializer.create(serializer.validated_data)
+            serializer.create(serializer.validated_data)
 
             content = {'New order added': serializer.validated_data}
             return Response(content)
